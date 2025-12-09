10f9f783459be7b64716b4110fd5a6f54160c7d2
maurice Mumbere <mauricendayango@gmail.com>
2025-12-10 00:46:59 +0100

    create ArticleItems component

---
+++ app/components/ArticlesItems.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+  article : Object
+})
+</script>
+
+<template>
+    <div class="bg-white rounded-t-xl shadow-lg">
+      <div>
+        <div>
+          <img class="h-[250px] rounded-t-xl" :src="article.image" alt="">
+        </div>
+        <div class="px-2 py-4">
+          <div class="py-2">
+            <h3>
+              {{article.author}}
+            </h3>
+          </div>
+          <div class="pb-2">
+            <h2 class="font-bold text-xl">
+              {{article.title}}
+            </h2>
+          </div>
+          <p>
+            {{article.description}}
+          </p>
+        </div>
+      </div>
+    </div>
+</template>
