d71cfc7c0dd93c76c2fde91b8b3373c75fa51d74
maurice Mumbere <mauricendayango@gmail.com>
2025-12-10 00:47:31 +0100

    create CategoryItems component

---
+++ app/components/CategoryItems.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  defineProps({
+    category : Object
+  })
+</script>
+
+<template>
+    <div>
+      <div class="flex items-center justify-center md:justify-start">
+        <img :src="category.image" alt="">
+      </div>
+      <h3 class="py-8">{{category.title}}</h3>
+      <p>{{category.description}}</p>
+    </div>
+</template>
