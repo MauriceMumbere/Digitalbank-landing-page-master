886cab1efcef55ee4e724e033e1387ce5609274c
maurice Mumbere <mauricendayango@gmail.com>
2025-12-10 00:48:56 +0100

    create Hero component

---
+++ app/components/Hero.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <div class="flex flex-col-reverse md:flex-row md:justify-between h-screen items-center md:pl-32 px-8 py-6 md:px-0 md:py-0">
+
+      <div class="flex-1 space-y-[2rem] text-center md:text-left">
+        <h1 class="text-4xl md:text-[4rem] leading-14 md:leading-20">Next generation digital banking</h1>
+        <p class="max-w-[27rem]"> Take your financial life online. Your Digitalbank account will be a one-stop-shop for
+          spending, saving, budgeting, investing, and much more. </p>
+        <div class=" pb-5"><a href="#" class="bg-emerald-300 py-2 px-8 rounded-4xl">Request Invite</a></div>
+      </div>
+
+      <div
+          class="flex-2 relative bg-[url('/images/bg-intro-desktop.svg')] bg-cover bg-no-repeat bg-position-[right_-26rem_bottom_2.4rem]">
+        <div class=" w-fit"><img class="md:ml-[18rem] md:mt-[4rem]" src="/images/image-mockups.png" alt="Image mockups">
+        </div>
+      </div>
+
+    </div>
+  </section>
+</template>
