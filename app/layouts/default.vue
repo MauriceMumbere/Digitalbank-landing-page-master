9be7354f27cebc22f3bdb74010902dec29b13a1b
maurice Mumbere <mauricendayango@gmail.com>
2025-12-11 11:09:28 +0100

    refactor: update head meta

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: "Frontend Mentor | Loopstudios landing page",
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/images/favicon-32x32.png",
+    },
+  ],
+});
+</script>
 
 <template>
   <div>
